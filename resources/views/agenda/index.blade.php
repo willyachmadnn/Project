81c122377897bafadbff106bfe044bffd796f44b
Anindyaoo <aniendyao@gmail.com>
2025-08-14 09:49:38 +0700

    Update Table

--- resources/views/agenda/index.blade.php
+++ resources/views/agenda/index.blade.php
@@ -73,11 +73,9 @@
                     </ul>
                 </div>
             @endif
-
             {{-- Tabel Agenda --}}
             <x-table :agendas="$agendas" />
         </div>
-
         <!-- Modal untuk Tambah Agenda -->
         <div x-show="isCreateModalOpen" @keydown.escape.window="isCreateModalOpen = false" class="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true" style="display: none;">
             <div class="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
