7227ee4b56225af7b1ba8f936652197311fcf281
Willy <willeste25@gmail.com>
2025-09-01 15:53:04 +0700

    Update Bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,12 +7,18 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'cache.headers' => \App\Http\Middleware\CacheHeaders::class,
+        ]);
+        
+        // Exclude CSRF verification for public tamu routes
+        $middleware->validateCsrfTokens(except: [
+            '/tamu/create',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
